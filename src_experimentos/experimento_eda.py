a5da48ae415765369f5a73921a319ce11bfaef4a
CLCHRISLOMBARDO <christian.lombardo14@gmail.com>
2025-11-01 13:45:19 -0300

    correccion de std

--- src_experimentos/experimento_eda.py
+++ src_experimentos/experimento_eda.py
@@ -17,7 +17,7 @@ def lanzar_eda():
                   name_pdf="reporte_medias_por_mes.pdf",
                   titulo="Medias por mes — Scatter por variable")
     
-    std_por_mes = std_por_mes(df)
-    crear_reporte_pdf(std_por_mes, xcol='foto_mes', columnas_y=std_por_mes.columns,
+    variacion_por_mes = std_por_mes(df)
+    crear_reporte_pdf(variacion_por_mes, xcol='foto_mes', columnas_y=variacion_por_mes.columns,
                   name_pdf="reporte_medias_por_mes.pdf",
                   titulo="Medias por mes — Scatter por variable")
