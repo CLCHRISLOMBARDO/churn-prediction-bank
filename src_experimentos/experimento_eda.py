28b2426966b66954d599771e7160dc7b524f9aa0
CLCHRISLOMBARDO <christian.lombardo14@gmail.com>
2025-11-04 23:36:02 -0300

    eda bajas por mes correccion

--- src_experimentos/experimento_eda.py
+++ src_experimentos/experimento_eda.py
@@ -13,18 +13,18 @@ def lanzar_eda(competencia:str|int):
     df= pl.read_csv(FILE_INPUT_DATA, infer_schema_length=10000)
     logger.info(df["foto_mes"].unique())
     filtros_target=("BAJA+1","BAJA+2")
-    media_por_mes = mean_por_mes(df=df , name_eda=name_eda, filtros_target=filtros_target)
+    media_por_mes = mean_por_mes(df=df , name=name_eda, filtros_target=filtros_target)
 
     crear_reporte_pdf(media_por_mes, xcol='foto_mes', columnas_y=media_por_mes.columns,
                   name_eda=name_eda,
                   motivo="media_por_mes")
     
-    variacion_por_mes = std_por_mes(df)
+    variacion_por_mes = std_por_mes(df, filtros_target=filtros_target)
     crear_reporte_pdf(variacion_por_mes, xcol='foto_mes', columnas_y=variacion_por_mes.columns,
                   name_eda=name_eda,
                   motivo="std_por_mes")
     
-    num_uniques_por_mes = nunique_por_mes(df=df , name_eda=name_eda, filtros_target=filtros_target)
+    num_uniques_por_mes = nunique_por_mes(df=df , name=name_eda, filtros_target=filtros_target)
     crear_reporte_pdf(num_uniques_por_mes, xcol='foto_mes', columnas_y=num_uniques_por_mes.columns,
                   name_eda=name_eda,
                   motivo="nunique_por_mes")
