3ad95a118f1418f6057f057fa3d82b21f23b20a4
CLCHRISLOMBARDO <christian.lombardo14@gmail.com>
2025-11-11 20:25:18 -0300

    correcion

--- src_feateng/feat_eng_1.py
+++ src_feateng/feat_eng_1.py
@@ -5,7 +5,7 @@ import json
 from src.config import *
 from src.configuracion_inicial import creacion_df_small
 from src.constr_lista_cols import contruccion_cols,cols_a_dropear_variable_entera,cols_a_dropear_variable_por_feat ,cols_a_dropear_variable_originales_o_percentiles,cols_a_dropear_variable_originales_o_corregidas ,cols_conteo_servicios_productos,cols_beneficios_presion_economica
-from src.feature_engineering import copia_tabla_vm_a_buckert,copia_tabla,feature_engineering_correccion_variables_por_mes_por_media,suma_de_prod_servs,suma_ganancias_gastos,ratios_ganancia_gastos,feature_engineering_percentil,feature_engineering_lag,feature_engineering_delta,feature_engineering_max_min,feature_engineering_ratio,feature_engineering_linreg,feature_engineering_drop_cols,feature_engineering_drop_meses
+from src.feature_engineering import copia_tabla_local_a_bucket,copia_tabla,feature_engineering_correccion_variables_por_mes_por_media,suma_de_prod_servs,suma_ganancias_gastos,ratios_ganancia_gastos,feature_engineering_percentil,feature_engineering_lag,feature_engineering_delta,feature_engineering_max_min,feature_engineering_ratio,feature_engineering_linreg,feature_engineering_drop_cols,feature_engineering_drop_meses
 ## ---------------------------------------------------------Configuraciones Iniciales -------------------------------
 ## Carga de variables
 
@@ -19,7 +19,7 @@ def lanzar_feat_eng(fecha:str ,n_fe:int , proceso_ppal:str):
     logger.info(f"Comienzo del experimento : {name}")
 
     # COPIA PARA EL BUCKETS A ELIMINAR ======================================
-    copia_tabla_vm_a_buckert()
+    copia_tabla_local_a_bucket()
     return
     # =========================================
     #DROPEO INICIAL DE MESES
