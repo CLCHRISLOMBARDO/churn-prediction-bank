c080d7aa6a77ad775d11dc2a3cf7995fbf7db430
CLCHRISLOMBARDO <christian.lombardo14@gmail.com>
2025-11-15 17:43:09 -0300

    f

--- visualizar_tablas.py
+++ visualizar_tablas.py
@@ -1,5 +1,6 @@
 import duckdb
+from src.config import *
 
-conn = duckdb.connect("PATH_A_TU_ARCHIVO.duckdb")
+conn = duckdb.connect(PATH_DATA_BASE_DB)
 print(conn.execute("SHOW TABLES").fetchdf())
 conn.close()
