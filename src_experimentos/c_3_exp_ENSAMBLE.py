6f391c7c750801909f3c030619a10f364f8a4765
CLCHRISLOMBARDO <christian.lombardo14@gmail.com>
2025-12-02 01:09:41 -0300

    comienzo de ENSAMBLES 12

--- src_experimentos/c_3_exp_ENSAMBLE.py
+++ src_experimentos/c_3_exp_ENSAMBLE.py
@@ -52,9 +52,9 @@ def lanzar_experimento_lgbm(fecha:str ,semillas:list[int],n_experimento:int,proc
     cantidad_trials= N_TRIALS
     cantidad_boosts = N_BOOSTS
     #"""-----------------------------------------------------------------------------------------------"""
-    names_exp_finals_preds=["..comp3_conf3_exp301b","..comp3_conf3_exp302","..comp3_conf3_exp303","..comp3_conf3_exp315a"]
-    n_semillas= ["50","50","50","50"]
-    numero_del_ensamble = "11_"
+    names_exp_finals_preds=["..comp3_conf3_exp301b","..comp3_conf3_exp302","..comp3_conf3_exp313"]
+    n_semillas= ["50","50","50"]
+    numero_del_ensamble = "12_"
 
     from functools import reduce
 
