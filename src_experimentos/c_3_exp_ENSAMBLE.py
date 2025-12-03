33aaa42b2d326af56010724259968c695052cd90
CLCHRISLOMBARDO <christian.lombardo14@gmail.com>
2025-12-03 18:55:18 -0300

    ensamble 1

--- src_experimentos/c_3_exp_ENSAMBLE.py
+++ src_experimentos/c_3_exp_ENSAMBLE.py
@@ -32,7 +32,7 @@ def lanzar_experimento_lgbm(fecha:str ,semillas:list[int],n_experimento:int,proc
     "..comp3_conf3_exp314c",
     "..comp3_conf3_exp315a",
     "..comp3_conf3_exp316",
-    "..comp3_conf3_exp321","..comp3_conf3_exp321"])
+    "..comp3_conf3_exp320","comp3_conf3_exp321"])
     #"""---------------------- CAMBIAR INPUTS --------------------------------------------------------"""
     numero=n_experimento
     #"""----------------------------------------------------------------------------------------------"""
